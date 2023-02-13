03b79225935054d39101bbb0325e9ecd90e2a66d
chojh3123 <chojh3123@gmail.com>
2023-02-13 22:34:30 +0900

    HazelcastCacheManager 등록

--- src/main/java/com/example/hazelcast/config/CacheConfig.java
+++ src/main/java/com/example/hazelcast/config/CacheConfig.java
@@ -1,8 +1,23 @@
 package com.example.hazelcast.config;
 
 
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.spring.cache.HazelcastCacheManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class CacheConfig {
+
+    private final HazelcastInstance hazelcastInstance;
+
+    @Bean
+    public CacheManager cacheManager(){
+        return new HazelcastCacheManager(hazelcastInstance);
+    }
+
+
 }
